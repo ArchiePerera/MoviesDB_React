793d91d13af3f8fee29da5ecd980d70201d69310
ArchiePerera <archivaldoperera@gmail.com>
2024-03-10 19:45:14 +0100

    cards system

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,19 +4,26 @@ const Card = ({data}) => {
 
     return (
         <>
+        <div className="Cards">
         {data && data.map((p, i) => (
-        <div  key={i} className="Card">
-            <h2>{p.title}</h2>
-            <p>{p.vote_count} votes</p>
-            <img src={`https://image.tmdb.org/t/p/original/${p.backdrop_path}`} alt={p.title} width={300} />
-            <img src={`https://image.tmdb.org/t/p/original/${p.poster_path}`} alt={p.title} width={300} />
-            <p>Date de sortie : {new Date(p.release_date).toLocaleDateString()}</p>
-            <h3>Résumé :</h3>
-            <p>{p.overview}</p>
-            <p>Note moyenne : {p.vote_average}</p>
+        <div  key={i} className="Card" style={{
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${p.backdrop_path}")`
+        }}>
+            <div className="movie-title">
+                <h2>{p.title}</h2>
+                <img src={`https://image.tmdb.org/t/p/original/${p.poster_path}`} alt={p.title} width={300} />
+            </div>
+            <div className="movie-details">
+                <p>Date de sortie : {new Date(p.release_date).toLocaleDateString()}</p>
+                <h3>Résumé :</h3>
+                <p>{p.overview}</p>
+                <p>{p.vote_count} votes</p>
+                <p>Note moyenne : {p.vote_average}</p>
+            </div>
+            <span></span>
         </div>
-        
-        ))}      
+        ))}
+        </div>     
         </>
     )
 }
